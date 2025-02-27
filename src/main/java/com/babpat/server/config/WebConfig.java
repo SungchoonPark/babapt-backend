3cb4c185e7692ee4766875255df6e5408b5ea473
박성춘 <songchoon10@naver.com>
2025-02-27 13:21:09 +0900

    refactor: 배포 도메인 cors 추가

--- src/main/java/com/babpat/server/config/WebConfig.java
+++ src/main/java/com/babpat/server/config/WebConfig.java
@@ -9,7 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // 프론트엔드 주소
+                .allowedOrigins(
+                        "http://localhost:3000", // 로컬
+                        "https://www.babpat.com" // 배포
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
