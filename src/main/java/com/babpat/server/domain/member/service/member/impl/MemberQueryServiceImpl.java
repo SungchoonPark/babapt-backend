45160981547824f374f58df80a4e4bd13d6376f5
박성춘 <songchoon10@naver.com>
2025-03-17 09:26:49 +0900

    refactor: id 중복체크시 필드명 id -> username 으로 변경

--- src/main/java/com/babpat/server/domain/member/service/member/impl/MemberQueryServiceImpl.java
+++ src/main/java/com/babpat/server/domain/member/service/member/impl/MemberQueryServiceImpl.java
@@ -15,7 +15,7 @@ public class MemberQueryServiceImpl implements MemberQueryService {
     private final MemberRepository memberRepository;
     @Override
     public IdCheckRespDto isExistId(IdCheckRequestDto requestDto) {
-        boolean isExist = memberRepository.existsByUsername(requestDto.id());
+        boolean isExist = memberRepository.existsByUsername(requestDto.username());
         return new IdCheckRespDto(isExist);
     }
 }
