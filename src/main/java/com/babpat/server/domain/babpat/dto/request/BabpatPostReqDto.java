dc9dc746d7604342782a5eaede6c70f52ac2ac25
박성춘 <songchoon10@naver.com>
2025-04-03 20:08:39 +0900

    refactor(#21): 밥팟 생성 로직에서 `밥팟`과 `도시락팟`이 구분되어 생성되도록 로직 변경

--- src/main/java/com/babpat/server/domain/babpat/dto/request/BabpatPostReqDto.java
+++ src/main/java/com/babpat/server/domain/babpat/dto/request/BabpatPostReqDto.java
@@ -3,33 +3,35 @@ package com.babpat.server.domain.babpat.dto.request;
 import com.babpat.server.domain.babpat.entity.Babpat;
 import com.babpat.server.domain.babpat.entity.enums.BabpatStatus;
 import com.babpat.server.domain.babpat.entity.enums.MealSpeed;
+import com.babpat.server.domain.babpat.entity.enums.PatType;
 import com.babpat.server.domain.member.entity.Member;
 import com.babpat.server.domain.restaurant.entity.Restaurant;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 public record BabpatPostReqDto(
-        @NotNull Long place,
-        @NotNull LocalDate date,
-        @NotNull LocalTime time,
-        @NotNull Integer headCount,
-        @NotBlank String comment,
-        String mealSpeed
+    @NotNull Long place,
+    @NotNull LocalDate date,
+    @NotNull LocalTime time,
+    @NotNull Integer headCount,
+    @NotBlank String comment,
+    String mealSpeed,
+    @NotBlank String patType
 ) {
 
-    public Babpat toBabpat(Member leader, Restaurant restaurant) {
-        return Babpat.builder()
-                .member(leader)
-                .restaurant(restaurant)
-                .comment(comment)
-                .headCount(headCount)
-                .patDate(date)
-                .patTime(time)
-                .mealSpeed(mealSpeed == null ? null : MealSpeed.fromString(mealSpeed))
-                .babpatStatus(BabpatStatus.ONGOING)
-                .build();
-    }
+  public Babpat toBabpat(Member leader, Restaurant restaurant) {
+    return Babpat.builder()
+        .member(leader)
+        .restaurant(restaurant)
+        .comment(comment)
+        .headCount(headCount)
+        .patDate(date)
+        .patTime(time)
+        .mealSpeed(mealSpeed == null ? null : MealSpeed.fromString(mealSpeed))
+        .babpatStatus(BabpatStatus.ONGOING)
+        .patType(PatType.fromString(patType))
+        .build();
+  }
 }
--- src/main/java/com/babpat/server/domain/babpat/entity/enums/PatType.java
+++ src/main/java/com/babpat/server/domain/babpat/entity/enums/PatType.java
@@ -1,6 +1,17 @@
 package com.babpat.server.domain.babpat.entity.enums;
 
+import com.babpat.server.common.enums.CustomResponseStatus;
+import com.babpat.server.common.exception.CustomException;
+
 public enum PatType {
   BABPAT,
-  DOSIRAK_PAT
+  DOSIRAK_PAT;
+
+  public static PatType fromString(String patType) {
+    return switch (patType.toLowerCase()) {
+      case "babpat" -> BABPAT;
+      case "dosirak_pat" -> DOSIRAK_PAT;
+      default -> throw new CustomException(CustomResponseStatus.INVALID_MEAL_SPEED);
+    };
+  }
 }
