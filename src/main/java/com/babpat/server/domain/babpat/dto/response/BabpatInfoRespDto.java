0d8f9a3e68ed0b9fcf726a42614415af0826cf84
박성춘 <songchoon10@naver.com>
2025-04-12 22:18:40 +0900

    refactor: 밥팟 및 도시락팟 모두 조회 가능하도록 변경.
    Detail
    - DOSIRAK_PAT -> DOSIRAK 으로 통일
    - 팟 조회시 밥팟인지 도시락팟인지 구분하는 컬럼 추가

--- src/main/java/com/babpat/server/domain/babpat/dto/response/BabpatInfoRespDto.java
+++ src/main/java/com/babpat/server/domain/babpat/dto/response/BabpatInfoRespDto.java
@@ -1,46 +1,55 @@
 package com.babpat.server.domain.babpat.dto.response;
 
 import com.babpat.server.domain.babpat.entity.enums.MealSpeed;
+import com.babpat.server.domain.babpat.entity.enums.PatType;
 import com.babpat.server.domain.member.entity.enums.Track;
 import com.babpat.server.domain.restaurant.dto.response.RestaurantInfo;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 public record BabpatInfoRespDto(
-        RestaurantInfo restaurantInfo,
-        BabpatInfo babpatInfo
+    RestaurantInfo restaurantInfo,
+    BabpatInfo babpatInfo
 ) {
 
 
-    public record BabpatInfo(
-            Long id,
-            String comment,
-            Capacity capacity,
-            MealSpeed mealSpeed,
-            LocalDate date,
-            LocalTime time,
-            LeaderProfile leaderProfile,
-            List<JoinMemberProfile> joinMemberProfiles
-    ) {}
+  public record BabpatInfo(
+      Long id,
+      String comment,
+      Capacity capacity,
+      MealSpeed mealSpeed,
+      LocalDate date,
+      LocalTime time,
+      PatType patType,
+      LeaderProfile leaderProfile,
+      List<JoinMemberProfile> joinMemberProfiles
+  ) {
 
-    public record Capacity(
-            int totalSlots,
-            int filledSlots
-    ) {}
+  }
 
-    public record LeaderProfile(
-            Long leaderId,
-            String name,
-            String nickname,
-            Track track
-    ) {}
+  public record Capacity(
+      int totalSlots,
+      int filledSlots
+  ) {
 
-    public record JoinMemberProfile(
-            Long id,
-            String name,
-            String nickname,
-            Track track
-    ) {}
+  }
+
+  public record LeaderProfile(
+      Long leaderId,
+      String name,
+      String nickname,
+      Track track
+  ) {
+
+  }
+
+  public record JoinMemberProfile(
+      Long id,
+      String name,
+      String nickname,
+      Track track
+  ) {
+
+  }
 }
